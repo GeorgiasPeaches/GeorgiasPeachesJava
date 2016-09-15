cf299a03c981455364ef7b0108f1ac1bdb999b3f
Collin Craig <admin@collincoding.com>
2016-09-15 15:30:24 -0400

    Made some things more simple

--- core/src/com/georgiaspeaches/game/CharterHallways.java
+++ core/src/com/georgiaspeaches/game/CharterHallways.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
-//60*80 grid
+//80*55 grid
 public class CharterHallways implements ApplicationListener {
 	public SpriteBatch batch;
 	public BitmapFont font;
@@ -30,7 +30,7 @@ public class CharterHallways implements ApplicationListener {
 		tile1render.width = 16;
 		tile1render.height = 16;
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 1280, 960);
+		camera.setToOrtho(false, 1280, 880);
 	}
 
 	@Override
@@ -48,7 +48,7 @@ public class CharterHallways implements ApplicationListener {
 		batch.begin();
 		drawGrass();
 		drawBrick();
-		drawTiles();
+		//drawTiles();
 		batch.end();
 	}
 
@@ -66,41 +66,20 @@ public class CharterHallways implements ApplicationListener {
 
 	public void drawGrass()
 	{
-		for(int x = 0; x<80*16; x+=16)
+		for(int x = 0; x<=80; x++)
 		{
-			for(int y = 0; y < 60 * 16; y += 16)
+			for(int y = 0; y<=55; y++)
 			{
-				batch.draw(grass, x, y);
+				batch.draw(grass, getXValue(x), getYValue(y));
 			}
 		}
 	}
 	public void drawBrick()
 	{
-		int x = 32;
-		while(x <= 1280-32-16)
+		for(int x = 0; x<=80; x++)
 		{
-			batch.draw(brick, x, 960-64+16);
-			batch.draw(brick, x, 960-64);
-			x+=16;
-		}
-		x = 0;
-		while(x <= 6*16)
-		{
-			batch.draw(brick, 32, 960-64-x);
-			batch.draw(brick, 48, 960-64-x);
-			x+=16;
-		}
-		x = 4*16;
-		while(x <= 20*16)
-		{
-			batch.draw(brick, x, 960-10*16);
-			batch.draw(brick, x, 960-9*16);
-			x+=16;
-		}
-		for(int i = 32; i<900-5*16; i+=16)
-		{
-			batch.draw(brick, 21*16, i);
-			batch.draw(brick, 21*16+16, i);
+			batch.draw(brick, getXValue(x), getYValue(1));
+			batch.draw(brick, getXValue(x), getYValue(2));
 		}
 	}
 	public void drawTiles()
@@ -125,5 +104,15 @@ public class CharterHallways implements ApplicationListener {
 			batch.draw(tile1, 23*16+64, i);
 			batch.draw(tile1, 23*16+80, i);
 		}
+	}
+	public int getYValue(int y)
+	{
+		y = 880 - 16*y;
+		return y;
+	}
+	public int getXValue(int x)
+	{
+		x = 16*x;
+		return x-16;
 	}
 }
--- desktop/src/com/georgiaspeaches/game/desktop/DesktopLauncher.java
+++ desktop/src/com/georgiaspeaches/game/desktop/DesktopLauncher.java
@@ -3,14 +3,13 @@ package com.georgiaspeaches.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.georgiaspeaches.game.CharterHallways;
-import com.georgiaspeaches.game.CharterHallways;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Charter Halls";
 		config.width = 1280;
-		config.height = 960;
+		config.height = 880;
 		new LwjglApplication(new CharterHallways(), config);
 	}
 }
