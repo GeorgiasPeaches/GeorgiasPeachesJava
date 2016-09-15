d22f5affbd4fea2db651653817bd507b84a93f0e
Collin Craig <admin@collincoding.com>
2016-09-15 14:21:49 -0400

    fuckyoukids

--- core/src/com/georgiaspeaches/game/CharterHallways.java
+++ core/src/com/georgiaspeaches/game/CharterHallways.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
-
+//60*80 grid
 public class CharterHallways implements ApplicationListener {
 	public SpriteBatch batch;
 	public BitmapFont font;
@@ -30,7 +30,7 @@ public class CharterHallways implements ApplicationListener {
 		tile1render.width = 16;
 		tile1render.height = 16;
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 1200, 900);
+		camera.setToOrtho(false, 1280, 960);
 	}
 
 	@Override
@@ -48,7 +48,7 @@ public class CharterHallways implements ApplicationListener {
 		batch.begin();
 		drawGrass();
 		drawBrick();
-		batch.draw(tile1, 64, 64);
+		drawTiles();
 		batch.end();
 	}
 
@@ -66,28 +66,64 @@ public class CharterHallways implements ApplicationListener {
 
 	public void drawGrass()
 	{
-		int x = 0;
-		while(x <= 1200)
+		for(int x = 0; x<80*16; x+=16)
 		{
-			batch.draw(grass, x, 0);
-			batch.draw(grass, x, 16);
-			batch.draw(grass, x, 900-16);
-			batch.draw(grass, x, 900-32);
-			batch.draw(grass, 0, x);
-			batch.draw(grass, 16, x);
-			batch.draw(grass, 1200-16, x);
-			batch.draw(grass, 1200-32, x);
-			x+=16;
+			for(int y = 0; y < 60 * 16; y += 16)
+			{
+				batch.draw(grass, x, y);
+			}
 		}
 	}
 	public void drawBrick()
 	{
 		int x = 32;
-		while(x <= 1200-32-16)
+		while(x <= 1280-32-16)
 		{
-			batch.draw(brick, x, 900-64+16);
-			batch.draw(brick, x, 900-64);
+			batch.draw(brick, x, 960-64+16);
+			batch.draw(brick, x, 960-64);
 			x+=16;
+		}
+		x = 0;
+		while(x <= 6*16)
+		{
+			batch.draw(brick, 32, 960-64-x);
+			batch.draw(brick, 48, 960-64-x);
+			x+=16;
+		}
+		x = 4*16;
+		while(x <= 20*16)
+		{
+			batch.draw(brick, x, 960-10*16);
+			batch.draw(brick, x, 960-9*16);
+			x+=16;
+		}
+		for(int i = 32; i<900-5*16; i+=16)
+		{
+			batch.draw(brick, 21*16, i);
+			batch.draw(brick, 21*16+16, i);
+		}
+	}
+	public void drawTiles()
+	{
+		int x = 32+32;
+		while(x <= 1280-32-16-36)
+		{
+			batch.draw(tile1, x, 960-64+16-32);
+			batch.draw(tile1, x, 960-64-32);
+			batch.draw(tile1, x, 960-64-32-16);
+			batch.draw(tile1, x, 960-64-32-32);
+			batch.draw(tile1, x, 960-64-32-48);
+			batch.draw(tile1, x, 960-64-32-64);
+			x+=16;
+		}
+		for(int i = 64; i<900-5*16; i+=16)
+		{
+			batch.draw(tile1, 23*16, i);
+			batch.draw(tile1, 23*16+16, i);
+			batch.draw(tile1, 23*16+32, i);
+			batch.draw(tile1, 23*16+48, i);
+			batch.draw(tile1, 23*16+64, i);
+			batch.draw(tile1, 23*16+80, i);
 		}
 	}
 }
--- desktop/src/com/georgiaspeaches/game/desktop/DesktopLauncher.java
+++ desktop/src/com/georgiaspeaches/game/desktop/DesktopLauncher.java
@@ -9,8 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Charter Halls";
-		config.width = 1200;
-		config.height = 900;
+		config.width = 1280;
+		config.height = 960;
 		new LwjglApplication(new CharterHallways(), config);
 	}
 }
