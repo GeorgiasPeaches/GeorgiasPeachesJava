cee04ca727f060e8e9a1c2de59f0dbb661ec344b
Collin Craig <admin@collincoding.com>
2016-09-21 08:15:24 -0400

    I did some stuff

--- core/src/com/georgiaspeaches/game/entities/Player.java
+++ core/src/com/georgiaspeaches/game/entities/Player.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -11,11 +12,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Player
 {
 	SpriteBatch sb;
-	Texture texture;
 	Sprite sprite;
+	Texture texture;
 	float myX = 58*16, myY = 97*16, speed = 80f;
 	int x = (int)myX/16;
 	int y = (int)myY/16;
+	SpriteBatch spriteBatch;
+	BitmapFont font;
+	CharSequence str = "Cheats Enabled [NOCLIP]";
 
 	public Player()
 	{
@@ -23,7 +27,11 @@ public class Player
 		sb = new SpriteBatch();
 		texture = new Texture(Gdx.files.internal("maps/piq.png"));
 		sprite = new Sprite(texture);
+		font = new BitmapFont();
 		sprite.setPosition(myX, myY);
+		spriteBatch = new SpriteBatch();
+		font = new BitmapFont();
+
 	}
 
 	public void render(OrthographicCamera camera)
@@ -40,23 +48,34 @@ public class Player
 		int currentY = (int)myY/16;
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
 		{
-			if(!mapArray[currentX-1][currentY])
+			if(!mapArray[currentX-1][currentY] || Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
 				myX -= Gdx.graphics.getDeltaTime() * speed;
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
 		{
-			if(!mapArray[currentX+1][currentY])
+			if(!mapArray[currentX+1][currentY] || Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
 				myX += Gdx.graphics.getDeltaTime() * speed;
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.UP))
 		{
-			if(!mapArray[currentX][currentY+1])
+			if(!mapArray[currentX][currentY+1] || Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
 				myY += Gdx.graphics.getDeltaTime() * speed;
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
 		{
-			if(!mapArray[currentX][currentY-1])
+			if(!mapArray[currentX][currentY-1] || Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
 				myY -= Gdx.graphics.getDeltaTime() * speed;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
+		{
+			speed = 150f;
+			spriteBatch.begin();
+			font.draw(spriteBatch, str, 100, 500);
+			spriteBatch.end();
+		}
+		else
+		{
+			speed = 80f;
 		}
 	}
 	public void setPos(float x, float y)
